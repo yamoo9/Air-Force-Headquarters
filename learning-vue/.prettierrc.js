49ff1f1039b2842c9dfaf169e9855a95cdd34886
yamoo9 <yamoo9@naver.com>
2019-03-22 17:31:18 +0900

    프로젝트 생성 및 환경설정

---
+++ learning-vue/.prettierrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  // 줄 바꿈 길이 설정
+  printWidth: 80,
+  // 탭(true), 스페이스(false) 사용 설정
+  useTabs: false,
+  // 탭(tab) 공백 수 설정
+  tabWidth: 2,
+  // 작은 따옴표(') 사용 설정
+  singleQuote: true,
+  // 명령문 끝에 세미콜론 추가 설정
+  semi: !true,
+  // 화살표 함수 => 단일 매개변수 괄호 () 설정
+  arrowParens: 'always',
+  // 객체 또는 배열의 여러 줄 입력 시, 후행 쉼표 추가 설정
+  trailingComma: 'es5',
+  // HTML 공백 감도 설정
+  htmlWhitespaceSensitivity: 'ignore',
+  // 객체 리터럴 괄호({ }) 사이 공백 출력
+  bracketSpacing: true,
+}
---
+++ learning-vue/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" alt="Vue logo" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+}
+</script>
+
+<style lang="scss"></style>
