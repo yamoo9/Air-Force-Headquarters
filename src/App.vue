de03bdf954571022a332858da10876d1cf6cda31
yamoo9 <yamoo9@naver.com>
2019-03-23 17:08:47 +0900

    선언적 렌더링, 속성 바인딩, 모듈 로드

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
     <header class="app-header">
       <h1 class="brand">
-        <a href="https://www.ediya.com/" target="_blank">
-          <span class="a11y-hidden">EDIYA COFFEE</span>
+        <a v-bind:href="brand.href" target="_blank">
+          <span class="a11y-hidden">{{ brand.title }}</span>
         </a>
       </h1>
       <button type="button" class="button is-open">
@@ -11,12 +11,36 @@
       </button>
       <nav hidden class="app-navigation">
         <ul class="reset-list">
-          <li><a href="#">메뉴</a></li>
-          <li><a href="#">이디야멤버스</a></li>
-          <li><a href="#">이디야커피랩</a></li>
-          <li><a href="#">이디야컬쳐랩</a></li>
-          <li><a href="#">공지사항</a></li>
-          <li><a href="#">매장찾기</a></li>
+          <li>
+            <a v-bind:href="navigation[0].href" target="_blank">
+              {{ navigation[0].text }}
+            </a>
+          </li>
+          <li>
+            <a v-bind:href="navigation[1].href" target="_blank">
+              {{ navigation[1].text }}
+            </a>
+          </li>
+          <li>
+            <a v-bind:href="navigation[2].href" target="_blank">
+              {{ navigation[2].text }}
+            </a>
+          </li>
+          <li>
+            <a v-bind:href="navigation[3].href" target="_blank">
+              {{ navigation[3].text }}
+            </a>
+          </li>
+          <li>
+            <a v-bind:href="navigation[4].href" target="_blank">
+              {{ navigation[4].text }}
+            </a>
+          </li>
+          <li>
+            <a v-bind:href="navigation[5].href" target="_blank">
+              {{ navigation[5].text }}
+            </a>
+          </li>
         </ul>
         <button
           type="button"
@@ -31,8 +55,17 @@
 </template>
 
 <script>
+import { data as ediyaNav } from './api/ediya-navigation.json'
+const { brand, navigation } = ediyaNav
+
 export default {
   name: 'app',
+  data() {
+    return {
+      brand,
+      navigation,
+    }
+  },
 }
 </script>
 
