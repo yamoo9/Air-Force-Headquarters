8bec836baa74d88fe4df1e4a725dc33142376601
yamoo9 <yamoo9@naver.com>
2019-03-25 14:39:55 +0900

    앱 헤더 컴포넌트로 분리

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,6 @@
 <template>
   <div id="app">
-    <header class="app-header">
-      <h1 class="brand">
-        <a v-if="checkBrandType" :href="brand.href" target="_blank">
-          <span class="a11y-hidden">{{ brand.title }}</span>
-        </a>
-        <a v-else :href="brand.href" class="brand-text">{{ brand.title }}</a>
-      </h1>
-      <app-navigation-open-button @click="onChangeStateNavigation" />
-      <nav class="app-navigation" :class="navigationClassViewState">
-        <ul class="reset-list">
-          <li v-for="item in navigation" :key="item.id">
-            <a :href="item.href" target="_blank">{{ item.text }}</a>
-          </li>
-        </ul>
-        <app-navigation-close-button @click="onChangeStateNavigation" />
-      </nav>
-    </header>
+    <app-header :brand="brand" :navigation="navigation"></app-header>
     <main class="app-main">
       <app-menu-list :menu="ediyaMenu"></app-menu-list>
     </main>
@@ -24,27 +8,23 @@
 </template>
 
 <script>
+import { data as ediyaMenu } from './api/ediya-menu.json'
 import { data as ediyaNav } from './api/ediya-navigation.json'
 const { brand, navigation } = ediyaNav
 
-import { data as ediyaMenu } from './api/ediya-menu.json'
-
-import appNavigationOpenButton from '@/components/appNavigationOpenButton.vue'
-import appNavigationCloseButton from '@/components/appNavigationCloseButton.vue'
+import appHeader from './components/appHeader.vue'
 import appMenuList from './components/appMenuList.vue'
 
 export default {
   name: 'app',
   components: {
-    appNavigationOpenButton,
-    appNavigationCloseButton,
+    appHeader,
     appMenuList,
   },
   data() {
     return {
       brand: { ...brand, type: 'image' },
       navigation,
-      isShowNavigation: false,
       ediyaMenu,
     }
   },
@@ -62,34 +42,5 @@ export default {
       }
     }
   },
-  computed: {
-    checkBrandType() {
-      return this.brand.type.includes('image')
-    },
-    navigationClassViewState() {
-      return this.isShowNavigation ? 'is-active' : null
-    },
-  },
-  methods: {
-    onChangeStateNavigation(value) {
-      this.isShowNavigation = value
-    },
-    onShowNavigation() {
-      this.isShowNavigation = true
-    },
-    onHideNavigation() {
-      this.isShowNavigation = false
-    },
-  },
 }
 </script>
-
-<style lang="sass">
-.brand
-  .brand-text
-    background-image: none
-    width: auto
-    font-size: 20px
-    font-weight: 900
-    color: #5372C4
-</style>
---
+++ src/components/appHeader.vue
@@ -0,0 +1,65 @@
+<template>
+  <header class="app-header">
+    <h1 class="brand">
+      <a v-if="checkBrandType" :href="brand.href" target="_blank">
+        <span class="a11y-hidden">{{ brand.title }}</span>
+      </a>
+      <a v-else :href="brand.href" class="brand-text">{{ brand.title }}</a>
+    </h1>
+    <app-button class="is-open" @click="onShowNavigation">
+      <img src="../assets/images/navigation-button.gif" alt="메뉴 열기" />
+    </app-button>
+    <nav class="app-navigation" :class="navigationClassViewState">
+      <ul class="reset-list">
+        <li v-for="item in navigation" :key="item.id">
+          <a :href="item.href" target="_blank">{{ item.text }}</a>
+        </li>
+      </ul>
+      <app-button
+        class="is-close-menu"
+        aria-label="메뉴 닫기"
+        @click="onHideNavigation"
+      >
+        ×
+      </app-button>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'appHeader',
+  props: ['brand', 'navigation'],
+  data() {
+    return {
+      isShowNavigation: false,
+    }
+  },
+  computed: {
+    checkBrandType() {
+      return this.brand.type.includes('image')
+    },
+    navigationClassViewState() {
+      return this.isShowNavigation ? 'is-active' : null
+    },
+  },
+  methods: {
+    onShowNavigation() {
+      this.isShowNavigation = true
+    },
+    onHideNavigation() {
+      this.isShowNavigation = false
+    },
+  },
+}
+</script>
+
+<style lang="sass">
+.brand
+  .brand-text
+    background-image: none
+    width: auto
+    font-size: 20px
+    font-weight: 900
+    color: #5372C4
+</style>
