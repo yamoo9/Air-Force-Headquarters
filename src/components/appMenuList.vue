3d61f0bd5d7e6c109b21dbf34bdf66e5dba6f18e
yamoo9 <yamoo9@naver.com>
2019-03-25 13:01:38 +0900

    메뉴 리스트 컴포넌트

---
+++ src/components/appMenuList.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul class="ediya-menu reset-list">
+    <li
+      is="app-menu-list-item"
+      v-for="item in menu"
+      :key="item.id"
+      :item="item"
+    />
+  </ul>
+</template>
+
+<script>
+import appMenuListItem from './appMenuListItem.vue'
+
+export default {
+  name: 'appMenuList',
+  components: { appMenuListItem },
+  props: ['menu'],
+}
+</script>
