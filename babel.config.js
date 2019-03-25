3403ffc58291fe8c96766c38e63f1339b2d196d0
yamoo9 <yamoo9@naver.com>
2019-03-26 01:54:25 +0900

    배포를 위한 build 명령 시 core-js 오류 문제 해결

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  presets: ['@vue/app'],
+  // 빌드 시, core-js 오류 발생할 경우
+  // 참고: https://cli.vuejs.org/guide/browser-compatibility.html#usebuiltins-usage
+  presets: [['@vue/app', { useBuiltIns: 'entry' }]],
 }
