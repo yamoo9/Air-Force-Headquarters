411f4b4ed1a60d085e61fbdba6136a162653b85f
yamoo9 <yamoo9@naver.com>
2019-03-26 00:17:23 +0900

    API 비동기 호출 + 라이프 사이클 훅

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,7 @@
 </template>
 
 <script>
-import { data as ediyaMenu } from './api/ediya-menu.json'
-import { data as ediyaNav } from './api/ediya-navigation.json'
-const { brand, navigation } = ediyaNav
+import axios from 'axios'
 
 import appHeader from './components/appHeader.vue'
 import appMenuList from './components/appMenuList.vue'
@@ -23,24 +21,54 @@ export default {
   },
   data() {
     return {
-      brand: { ...brand, type: 'image' },
-      navigation,
-      ediyaMenu,
+      brand: { type: 'image' },
+      navigation: [],
+      ediyaMenu: [],
     }
   },
   created() {
-    for (let { figure } of this.ediyaMenu) {
-      for (let key in figure) {
-        if (figure.hasOwnProperty(key) && key === 'name') {
-          this.$set(
-            figure,
-            'path',
-            require(`./assets/images/${figure[key]}.png`)
-          )
-          break
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      // API
+      // ------------------------------------------------------------
+      // ediya-menu.json       : https://api.myjson.com/bins/htzry
+      // ediya-navigation.json : https://api.myjson.com/bins/11jnm6
+      // ------------------------------------------------------------
+
+      // ediya-menu 비동기 통신
+      axios
+        .get('https://api.myjson.com/bins/htzry')
+        .then(({ data }) => data.data)
+        .then((data) => {
+          this.ediyaMenu = data
+          this.changeFigureImagePath()
+        })
+
+      // ediya-navigation 비동기 통신
+      axios
+        .get('https://api.myjson.com/bins/11jnm6')
+        .then(({ data }) => data.data)
+        .then(({ brand, navigation }) => {
+          this.brand = Object.assign(this.brand, brand)
+          this.navigation = navigation
+        })
+    },
+    changeFigureImagePath() {
+      for (let { figure } of this.ediyaMenu) {
+        for (let key in figure) {
+          if (figure.hasOwnProperty(key) && key === 'name') {
+            this.$set(
+              figure,
+              'path',
+              require(`./assets/images/${figure[key]}.png`)
+            )
+            break
+          }
         }
       }
-    }
+    },
   },
 }
 </script>
