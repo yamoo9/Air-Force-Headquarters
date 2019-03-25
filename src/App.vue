4dd7c773af41fc667ed83b0b6750aa0eaf1c85a3
yamoo9 <yamoo9@naver.com>
2019-03-26 00:53:09 +0900

    API 모듈 로드, API 코드 활용

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
   <div id="app">
-    <app-header :brand="brand" :navigation="navigation"></app-header>
+    <app-header :brand="brand" :navigation="navigation" />
     <main class="app-main">
-      <app-menu-list :menu="ediyaMenu"></app-menu-list>
+      <app-menu-list :menu="ediyaMenu" />
     </main>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-
+import api from './api'
 import appHeader from './components/appHeader.vue'
 import appMenuList from './components/appMenuList.vue'
 
@@ -31,29 +30,15 @@ export default {
   },
   methods: {
     fetchData() {
-      // API
-      // ------------------------------------------------------------
-      // ediya-menu.json       : https://api.myjson.com/bins/htzry
-      // ediya-navigation.json : https://api.myjson.com/bins/11jnm6
-      // ------------------------------------------------------------
-
-      // ediya-menu 비동기 통신
-      axios
-        .get('https://api.myjson.com/bins/htzry')
-        .then(({ data }) => data.data)
-        .then((data) => {
-          this.ediyaMenu = data
-          this.changeFigureImagePath()
-        })
-
-      // ediya-navigation 비동기 통신
-      axios
-        .get('https://api.myjson.com/bins/11jnm6')
-        .then(({ data }) => data.data)
-        .then(({ brand, navigation }) => {
-          this.brand = Object.assign(this.brand, brand)
-          this.navigation = navigation
-        })
+      const { getEdiyaMenu, getEdiyaNavigation } = api
+      getEdiyaMenu().then((data) => {
+        this.ediyaMenu = data
+        this.changeFigureImagePath()
+      })
+      getEdiyaNavigation().then(({ brand, navigation }) => {
+        this.brand = Object.assign(this.brand, brand)
+        this.navigation = navigation
+      })
     },
     changeFigureImagePath() {
       for (let { figure } of this.ediyaMenu) {
