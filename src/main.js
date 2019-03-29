fa5f0dccc0add23311e71bf7b88290bcc2223f93
yamoo9 <yamoo9@naver.com>
2019-03-29 11:25:56 +0900

    Vuex 설치 및 구성

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 import '@/assets/app.css'
 
 import Vue from 'vue'
+import store from './store'
 import App from './App.vue'
 
 // 글로벌 컴포넌트 파일 로드
@@ -10,5 +11,6 @@ import './plugins/globalComponents.js'
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: (h) => h(App),
 }).$mount('#app')
